3cc12dce763f82db740e11536a9b96fcb0b9b58f
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-11-13 17:13:45 +0700

    Fsss

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -1,23 +1,32 @@
 /* eslint-disable no-console */
 const fs = require("fs");
+const _ = require("lodash");
 const path = require("path");
 
 var faker = require("faker");
+
+var ranColor = ["red", "orange", "yellow", "green", "blue"];
+var ranSize = ["XS", "XS/S", "S", "S/M", "M"];
+var ranBrand = ["Louis Vuitton", "Lacoste", "Gucci", "Chanel", "Balenciaga"];
+
 function generateEmployees() {
   var products = [];
-  for (var id = 0; id < 50; id++) {
+  for (var id = 0; id < 100; id++) {
     var name = faker.name.title();
     var price = faker.commerce.price();
-    var description = faker.commerce.productDescription();
     let image = faker.image.fashion();
-    let size = faker.random.number();
+    let color = _.sample(ranColor);
+    let description = faker.lorem.words(50);
+
     products.push({
       id: id,
       name: name,
-      price: price,
       description: description,
+      price: price,
+      color: color,
       image: image,
-      size: size,
+      size: _.sample(ranSize),
+      brand: _.sample(ranBrand),
     });
   }
   return { products: products };
