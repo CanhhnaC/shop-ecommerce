ee22a2f05a29a6e74f33d1be5065b157847705dd
dangduyen110899 <duyendt@fimo.edu.vn>
2020-11-16 11:15:27 +0700

    detail

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -23,6 +23,7 @@ const StyleDetail = styled.div`
   .content {
     padding: 20px;
     width: 60%;
+    letter-spacing: 1.5px;
     h3 {
       font-size: 25px;
     }
@@ -32,21 +33,30 @@ const StyleDetail = styled.div`
     }
     h4 {
       font-size: 20px;
-      padding: 5px 0;
+    }
+
+    h5 {
+      text-transform: capitalize;
+    }
+    p {
+      font-size: 12px;
+      color: #666;
+      line-height: 25px;
+      letter-spacing: 1px;
     }
     .round {
-    width: 22px;
-    height: 22px;
+    width: 25px;
+    height: 25px;
     border-radius: 50%;
     border: 1px solid #000;
     position: relative;
-    margin: 10px 0 0 0;
+    margin: 15px 0 0 0;
     &::after {
     content: "";
     background-image: url(${({img}) => img});
     background-position: center;
-    width: 17px;
-    height: 17px;
+    width: 20px;
+    height: 20px;
     border-radius: 50%;
     cursor: pointer;
     position: absolute;
@@ -65,21 +75,36 @@ const StyleDetail = styled.div`
     }
 
     .detail {
+      position: relative;
+      border: 1px solid #ddd;
+      border-left: none;
+      border-right: none;
+      background-color: #fff;
       button {
         width: 100%;
         height: 50px;
         outline: none;
-        border: 1px solid;
-        border-left: none;
-        border-right: none;
+        border: none;
         text-align: left;
         padding-left: 20px;
+        background-color: #fff;
+      }
+
+      span {
+        position: absolute;
+        top: 13px;
+        right: 10px;
       }
     }
 
     .quantity {
+      font-weight: bold;
+      font-size: 20px;
+      height: 45px;
+      margin-top: 20px;
       button {
-        width: 40px;
+        border: 1px solid #ddd;
+        width: 50px;
         outline: none;
       }
 
@@ -92,10 +117,11 @@ const StyleDetail = styled.div`
       }
 
       input {
+        border: 1px solid #ddd;
         outline: none;
         border-left: none;
         border-right: none;
-        width: 40px;
+        width: 50px;
         text-align: center;
       }
     }
@@ -103,6 +129,9 @@ const StyleDetail = styled.div`
     .desc {
       display: ${({display}) => display ? "block": "none"};
       padding: 20px;
+      color: #666;
+      line-height: 25px;
+      letter-spacing: 1px;
     }
   }`
 const Detail = ({match}) => {
@@ -131,25 +160,33 @@ const Detail = ({match}) => {
         <h3>{product.brand}</h3>
         <h2>{product.name}</h2>
         <div style={{display: "flex", justifyContent: "space-between", margin: "20px 0"}}>
-          <h4>${product.price}</h4>
+          <h4>Price: ${product.price}</h4>
           <h5>Styled: {`${product.id}-${product.color}-${product.size}`}</h5>
         </div>
-        <h4>{product.color}</h4>
+        <h5>{product.color}</h5>
         <div className="round">
         </div>
         <div style={{display: "flex", margin: "20px 0", justifyContent: "space-between"}}>
-          <div className="quantity"  style={{display: "flex"}}>
-            <button className="decrement" onClick={() => {if (quatity > 1) {
-              setQuatity(quatity-1)
-            }}}>
-              <span>-</span>
-            </button>
-            <input type="tel" value={quatity}/>
-            <button  className="increment" onClick={() => setQuatity(quatity+1)}>
-              <span>+</span>
-            </button>
+          <div>
+            <label>Qty</label>
+            <div className="quantity"  style={{display: "flex"}}>
+              <button className="decrement" onClick={() => {if (quatity > 1) {
+                setQuatity(quatity-1)
+              }}}>
+                <span>-</span>
+              </button>
+              <input type="tel" value={quatity}/>
+              <button  className="increment" onClick={() => setQuatity(quatity+1)}>
+                <span>+</span>
+              </button>
           </div>
-          <Input data={RANDOM_SIZE} onChange={onSize} value={size} label="Size" width="120px" />
+          </div>
+          <div>
+            <label>Choose size</label>
+            <div style={{marginTop: "20px"}}>
+              <Input data={RANDOM_SIZE} onChange={onSize} value={size} label="Size" width="140px" />
+            </div>
+          </div>
         </div>
         <p>
           Eligible for a $2.62 donation to the cause of your choice. Select Cause | Learn More Powered By ShoppingGives
@@ -158,7 +195,8 @@ const Detail = ({match}) => {
           <Button>Add To Cart</Button>
         </div>
         <div className="detail">
-          <button onClick={() => setShowDetail(!show)}>Detail</button>
+          <button onClick={() => setShowDetail(!show)}>Detail & Care</button>
+          <span><i class="fas fa-chevron-down"></i></span>
           <p className="desc">
             {product.description}
           </p>
