88c29b9d35c29177e1dfd94011863698398bd7fc
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-11-17 08:14:41 +0700

    Fix link header, navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import gif_logo from "../../assets/images/logo-gif.PNG";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const Navbar = styled.div`
-    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
     font-weight: 400;
     font-size: 0.8rem;
     color: black;
@@ -11,9 +13,12 @@ const Navbar = () => {
     margin-top: 30px;
     justify-content: center;
     text-transform: uppercase;
-    div:hover {
-      color: gray;
-      cursor: pointer;
+    div a {
+      text-decoration: none;
+      &:hover {
+        color: gray;
+        cursor: pointer;
+      }
     }
     .icon {
       margin-top: -2.3%;
@@ -22,20 +27,39 @@ const Navbar = () => {
       margin-left: 30px;
     }
   `;
+
   return (
     <Navbar>
       <div className="icon">
         <img src={gif_logo} alt="Logo"></img>
       </div>
-      <div className="new_text">NEW!</div>
-      <div>BRAS &amp; Lingerie</div>
-      <div>Sleep &amp; Lounge</div>
-      <div>CloThing</div>
-      <div>Accessories</div>
-      <div>home</div>
-      <div>Sale</div>
-      <div>Explore</div>
-      <div>Rewards</div>
+      <div>
+        <Link to="/">NEW!</Link>
+      </div>
+      <div>
+        <Link to="/">BRAS &amp; Lingerie</Link>
+      </div>
+      <div>
+        <Link to="/">Sleep &amp; Lounge</Link>
+      </div>
+      <div>
+        <Link to="/">CloThing</Link>
+      </div>
+      <div>
+        <Link to="/">Accessories</Link>
+      </div>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/">Sale</Link>
+      </div>
+      <div>
+        <Link to="/">Explore</Link>
+      </div>
+      <div>
+        <Link to="/">Rewards</Link>
+      </div>
     </Navbar>
   );
 };
